698f4c0efc31d79c9117202218196a25be306cf1
Tanha <nusrattanha897@gmail.com>
2024-04-08 02:06:12 +0600

    comparison

---
+++ chai_aur_code/basics/comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true// 
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false
+//information: when comparing null with either undefined, 0, or "", it returns false.
+// console.log(null == 0); // false. 
+// console.log(null >= 0); //true
+
+
+//info: undefined does not coerced any other type. It only returns true if both sides are undefined.
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined >= 0); //false
+// console.log(null == undefined); //problem: the output is true. 
+
+// comparison and equality check both are different. 
+
+// strict check (===)
