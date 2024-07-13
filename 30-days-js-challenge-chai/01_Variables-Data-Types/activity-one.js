13198e477701f4a797b693a552cca8b1cb241ae9
Tanha <nusrattanha897@gmail.com>
2024-07-13 22:19:00 +0600

    completed activity one

---
+++ 30-days-js-challenge-chai/01_Variables-Data-Types/activity-one.js
@@ -0,0 +1,11 @@
+// problem: Task 01: declare a variable using var, assign it a number, and log the value to the console.
+
+var num;
+num = 10;
+console.log(num);
+
+// problem: Task 02: Declare a variable using let, assign it a string, and log the value to the console.
+
+let userName;
+userName = 'Tanha';
+console.log(userName);
