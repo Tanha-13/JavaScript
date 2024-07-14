0f9032617144d2e40a1106cbf40a760f2bc75275
Tanha <nusrattanha897@gmail.com>
2024-07-14 22:31:29 +0600

    activity 2 and 3 completed

---
+++ 30-days-js-challenge-chai/02_Operators/02_activity-two.js
@@ -0,0 +1,13 @@
+// problem: Use the `+=` operator to add a number to a variable and log the result to the console.
+
+let myAge = 24;
+myAge+=1;
+console.log(myAge); // 25
+
+// problem: Use the `-=` operator to subtract a number from a variable and log the result to the console.
+
+let yourNumber = 56;
+yourNumber -= 5;
+console.log(yourNumber); // 51
+
+
---
+++ 30-days-js-challenge-chai/02_Operators/03_activity-three.js
@@ -0,0 +1,14 @@
+//problem: Write a program to compare two numbers using `>` and `<` and log the result to the console.
+
+const number1 = 56;
+const number2 = 45;
+console.log(number1 > number2); //true
+console.log(number1 < number2); //false
+
+//problem: Write a program to compare two numbers using `>=` and `<=` and log the result to the console.
+
+//problem:  Write a program to compare two numbers using `==` and `===` and log the result to the console. 
+const num1 = 25;
+const num2 = '25';
+console.log(num1 == num2); // true
+console.log(num1 === num2); // false
