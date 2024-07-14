196e462e311508af85e26ecfe6f5fb33e6d0a8c9
Tanha <nusrattanha897@gmail.com>
2024-07-14 22:44:02 +0600

    allactivities are completed

---
+++ 30-days-js-challenge-chai/02_Operators/04_activity-four.js
@@ -0,0 +1,11 @@
+//problem: Write a program that uses the `&&` operator to combine two conditions and log the result to the console. 
+const myResult = 88;
+if(myResult >=80 && myResult < 90){
+    console.log('I have gotten "A+"');
+}
+
+
+// problem: Write a program that uses the `||` operator to combine two conditions and log the result to the console.
+
+
+//problem: Write a program that uses the `!` operator to negate a condition and log the result to the console. 
---
+++ 30-days-js-challenge-chai/02_Operators/05_activity-five.js
@@ -0,0 +1,10 @@
+// problem: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+const number1 = 45;
+const number2 = -45;
+const result1 = number1>=0 ?'number is positive' :'number is negative';
+const result2 = number2>=0 ?'number is positive': 'number is negative';
+console.log(result1); // number is positive
+console.log(result2); // number is negative
+
+
