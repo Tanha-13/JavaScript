e07ec6f028ab4501a6c80b410e905f43c50dd6b9
Tanha <nusrattanha897@gmail.com>
2024-07-14 22:10:03 +0600

    arithmetic operators tasks completed

---
+++ 30-days-js-challenge-chai/02_Operators/01_activity-one.js
@@ -0,0 +1,33 @@
+// problem: Write a program to add two numbers and log the result to the console.
+
+const num1 = 10;
+const num2 = 20;
+const addResult = num1 + num2;
+console.log(addResult);
+
+// problem: Write a program to subtract two numbers and log the result to the console.
+const sub1 = 3567;
+const sub2 = 345;
+const subResult = sub1 - sub2;
+console.log(subResult); // 3222
+
+//problem: Write a program to multiply two numbers and log the result to the console.
+
+const oneNumber = 45;
+const twoNumber = 76;
+const multiplyResult = oneNumber * twoNumber;
+console.log(multiplyResult); // 3420
+
+// problem: Write a program to divide two numbers and log the result to the console.
+
+const divNum1 = 987;
+const divNum2 = 45;
+const divideResult = divNum1 / divNum2;
+console.log(divideResult); // 21.9333333333334
+
+// problem: Write a program to find the reminder when one number is divided by another and log the result to the console.
+
+const number1 = 56;
+const number2 = 5;
+const reminderResult = number1 % number2;
+console.log(reminderResult); // 1
